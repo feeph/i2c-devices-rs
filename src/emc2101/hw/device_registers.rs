4974ea21ac1eee502acf9df7e210abf7bbe50cef
Feeph Aifeimei <55798703+feeph@users.noreply.github.com>
2025-08-03 23:07:09 +0200

    feat: WIP Sun Aug  3 11:07:09 PM CEST 2025

--- src/emc2101/hw/device_registers.rs
+++ src/emc2101/hw/device_registers.rs
@@ -5,13 +5,13 @@
 pub enum DR {
     Its = 0x00,      // internal sensor - temperature
     EtsMsb = 0x01,   // external diode - temperature (high byte)
-    ItsHi = 0x05,    // internal sensor - temperature high limit
-    EtsLoMsb = 0x08, // external diode - temperature low limit (high byte)
-    EtsLoLsb = 0x14, // external diode - temperature low limit (low byte)
-    EtsHiMsb = 0x07, // external diode - temperature high limit (high byte)
-    EtsHiLsb = 0x13, // external diode - temperature high limit (low byte)
     Cfg = 0x03,      // device config register
+    ItsHi = 0x05,    // internal sensor - temperature high limit
+    EtsHiMsb = 0x07, // external diode - temperature high limit (high byte)
+    EtsLoMsb = 0x08, // external diode - temperature low limit (high byte)
     EtsLsb = 0x10,   // external temperature sensor (low byte)
+    EtsHiLsb = 0x13, // external diode - temperature high limit (low byte)
+    EtsLoLsb = 0x14, // external diode - temperature low limit (low byte)
     Pid = 0xFD,      // product ID
     Mid = 0xFE,      // manufacturer ID
     Rev = 0xFF,      // product revision
